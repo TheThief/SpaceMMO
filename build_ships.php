00f1f7a0d21d65b03d944fce6ec7616df47cb904
TheThief <thief@dynamicarcade.co.uk>
2009-03-29 13:58:24 +0000

    Fixing up space-indenting to tabs

--- build_ships.php
+++ build_ships.php
@@ -16,36 +16,36 @@ function buildShipsBody()
 	$orderarray = array();
 	
 	$query = $mysqli->prepare('SELECT shipconstruction FROM colonies WHERE planetid = ?;');
-        if (!$query)
-        {
-                echo 'error: ', $mysqli->error, $eol;
-                exit;
-        }
-        $query->bind_param('i', $planetid);
-        $result = $query->execute();
-        if (!$result)
-        {
-                echo 'error: ', $query->error, $eol;
-                exit;
-        }
-        $query->bind_result($shipprod);
+	if (!$query)
+	{
+		echo 'error: ', $mysqli->error, $eol;
+		exit;
+	}
+	$query->bind_param('i', $planetid);
+	$result = $query->execute();
+	if (!$result)
+	{
+		echo 'error: ', $query->error, $eol;
+		exit;
+	}
+	$query->bind_result($shipprod);
 	$query->fetch();
 	$query->close();
 	
 	$query = $mysqli->prepare('SELECT shipname,count,buildprogress,metalcost,queueid FROM shipbuildqueue LEFT JOIN shipdesigns USING (designid) LEFT JOIN shiphulls USING(hullid) WHERE planetID = ? ORDER BY queueID ASC;');
-        if (!$query)
-        {
-                echo 'error: ', $mysqli->error, $eol;
-                exit;
-        }
-        $query->bind_param('i', $planetid);
+	if (!$query)
+	{
+		echo 'error: ', $mysqli->error, $eol;
+		exit;
+	}
+	$query->bind_param('i', $planetid);
 
-        $result = $query->execute();
-        if (!$result)
-        {
-                echo 'error: ', $query->error, $eol;
-                exit;
-        }
+	$result = $query->execute();
+	if (!$result)
+	{
+		echo 'error: ', $query->error, $eol;
+		exit;
+	}
 	$query->bind_result($designname,$count,$progress,$metalcost,$id);
 	?>
 	<h2>Build Queue</h2>
@@ -54,9 +54,9 @@ function buildShipsBody()
 	<?	
 	$timeoffset=0;
 	if($query->fetch())
-        {
-                do
-                {	
+	{
+		do
+		{	
 			if($shipprod>0){
 				$shipticks = ($metalcost-$progress)/$shipprod;
 				$shiptime = (ceil(($shipticks+$timeoffset))*600)-getTickElapsed();
@@ -67,19 +67,19 @@ function buildShipsBody()
 				$orderarray[$id]=$ordertime;
 			}
 			echo '<tr>';
-                        echo "<td>$designname</td>";
-                        echo "<td>$count</td>";
-                        echo "<td>".(int)(($progress/$metalcost)*100)."%</td>";
-                        echo "<td><span id=\"shsp".$id."\">-<span></td>";
-                        echo "<td><span id=\"orsp".$id."\">-<span></td>";
+			echo "<td>$designname</td>";
+			echo "<td>$count</td>";
+			echo "<td>".(int)(($progress/$metalcost)*100)."%</td>";
+			echo "<td><span id=\"shsp".$id."\">-<span></td>";
+			echo "<td><span id=\"orsp".$id."\">-<span></td>";
 			echo '</tr>';
 
-                } while ($query->fetch());
-        }
-        else
-        {
-                echo '<tr><td colspan="3">Empty</td></tr>';
-        }
+		} while ($query->fetch());
+	}
+	else
+	{
+		echo '<tr><td colspan="3">Empty</td></tr>';
+	}
 	?>
 	</table>
 	<h2>Ship Designs</h2>
@@ -136,41 +136,41 @@ function buildShipsBody()
 	?>
 	<script type="text/javascript">
 function reloadPage(){
-        location.reload(true);
+	location.reload(true);
 }
 function liveCount(seconds,name,first){
-        span=document.getElementById(name);
-        if(first==1){
-                //spanb=document.getElementById("b" + name);
-                var d = new Date();
-                d.setTime(d.getTime()+(seconds*1000));
-                span.title = d.toLocaleDateString() + " " + d.toLocaleTimeString();
-                //spanb.title = d.toLocaleDateString() + " " + d.toLocaleTimeString();
-        }
-        hours = Math.floor(seconds/3600);
-        minutes = Math.floor(seconds/60)%60;
-        sec = seconds%60;
-        span.innerHTML =  hours + ":" + padString(minutes,"0",2) + ":" + padString(sec,"0",2);
-        //span.title = hours + ":" + padString(minutes,"0",2) + ":" + padString(sec,"0",2);
-        if (seconds>0){
-                 setTimeout('liveCount('+(seconds - 1)+',"'+name+'",0);',1000);
-        }else{
-                 setTimeout('reloadPage()',10000);
-        }
+	span=document.getElementById(name);
+	if(first==1){
+		//spanb=document.getElementById("b" + name);
+		var d = new Date();
+		d.setTime(d.getTime()+(seconds*1000));
+		span.title = d.toLocaleDateString() + " " + d.toLocaleTimeString();
+		//spanb.title = d.toLocaleDateString() + " " + d.toLocaleTimeString();
+	}
+	hours = Math.floor(seconds/3600);
+	minutes = Math.floor(seconds/60)%60;
+	sec = seconds%60;
+	span.innerHTML =  hours + ":" + padString(minutes,"0",2) + ":" + padString(sec,"0",2);
+	//span.title = hours + ":" + padString(minutes,"0",2) + ":" + padString(sec,"0",2);
+	if (seconds>0){
+		 setTimeout('liveCount('+(seconds - 1)+',"'+name+'",0);',1000);
+	}else{
+		 setTimeout('reloadPage()',10000);
+	}
 
 }
 function padString(string,chr,len){
-        tempstring = string.toString();
-        while(tempstring.length < len) tempstring = chr + tempstring;
-        return tempstring;
+	tempstring = string.toString();
+	while(tempstring.length < len) tempstring = chr + tempstring;
+	return tempstring;
 }
 <?
 //print_r($countarray);
 foreach($shiparray as $cid => $ctime){
-        echo "liveCount(".$ctime.",\"shsp".$cid."\",1);";
+	echo "liveCount(".$ctime.",\"shsp".$cid."\",1);";
 }
 foreach($orderarray as $cid => $ctime){
-        echo "liveCount(".$ctime.",\"orsp".$cid."\",1);";
+	echo "liveCount(".$ctime.",\"orsp".$cid."\",1);";
 }
 
 ?>
