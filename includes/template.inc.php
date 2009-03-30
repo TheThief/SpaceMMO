91d12f85bd88ac19a79d4f09a09115a7e70b6bdf
TheThief <thief@dynamicarcade.co.uk>
2009-03-30 19:47:16 +0000

    New template. Hope this works!

--- includes/template.inc.php
+++ includes/template.inc.php
@@ -4,7 +4,7 @@ function template($title, $bodyfunc, $menufunc=null)
 	global $eol, $mysqli;
 	$userid = $_SESSION['userid'];
 
-	echo '<?xml version="1.0" encoding="utf-8"?>', $eol;
+	//echo '<?xml version="1.0" encoding="utf-8"?>', $eol;
 	//echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">', $eol;
 	//echo '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">', $eol;
 	echo '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">', $eol;
@@ -15,8 +15,9 @@ function template($title, $bodyfunc, $menufunc=null)
 	echo '</head>', $eol;
 	echo '<body>', $eol;
 	echo '', $eol;
+	echo '<div class="menuouter">', $eol;
 	echo '<div class="logo">', $eol;
-	echo '<img alt="Logo!" src="images/logo.png">', $eol;
+	echo '<img src="images/logo.png" alt="Logo!">', $eol;
 	echo '</div>', $eol;
 	echo '', $eol;
 	echo '<div class="menu">', $eol;
@@ -31,9 +32,15 @@ function template($title, $bodyfunc, $menufunc=null)
 		include(dirname(__FILE__).'/../admin/includes/adminmenu.inc.php');
 	}
 	echo '</div>', $eol;
+	echo '</div>', $eol;
 	echo '', $eol;
+	echo '<div class="bodyouter">', $eol;
+	echo '<div class="title">', $eol;
+	echo '<h1>', $title, '</h1>', $eol;
+	echo '</div>', $eol;
 	echo '<div class="body">', $eol;
 	$bodyfunc();
+	echo '</div>', $eol;
 	echo '</div>', $eol;
 	echo '', $eol;
 	echo '</body>', $eol;
