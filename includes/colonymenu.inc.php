3728934df5759ba56ec670179179cf4a91670dfc
TheThief <thief@dynamicarcade.co.uk>
2009-03-31 16:51:10 +0000

    1st pass view_ships.php

--- includes/colonymenu.inc.php
+++ includes/colonymenu.inc.php
@@ -1,1 +1,12 @@
-<?function colonyMenu(){	global $eol;	$planetid = $_GET['planet'];	echo '<h3>Colony Menu</h3>', $eol;	echo '<ul>', $eol;	echo '<li><a href="/SpaceMMO/colony_buildings.php?planet=', $planetid, '">Upgrade Buildings</a></li>', $eol;	echo '<li><a href="/SpaceMMO/build_ships.php?planet=', $planetid, '">Build Ships</a></li>', $eol;	echo '</ul>', $eol;}
+<?
+function colonyMenu()
+{
+	global $eol;
+	$planetid = $_GET['planet'];
+	echo '<h3>Colony Menu</h3>', $eol;
+	echo '<ul>', $eol;
+	echo '<li><a href="/SpaceMMO/colony_buildings.php?planet=', $planetid, '">Upgrade Buildings</a></li>', $eol;
+	echo '<li><a href="/SpaceMMO/build_ships.php?planet=', $planetid, '">Build Ships</a></li>', $eol;
+	echo '<li><a href="/SpaceMMO/view_ships.php?planet=', $planetid, '">Ships in Orbit</a></li>', $eol;
+	echo '</ul>', $eol;
+}
---
+++ view_ships.php
@@ -0,0 +1,51 @@
+<?
+include 'includes/start.inc.php';
+checkLoggedIn();
+
+include 'includes/template.inc.php';
+template('Ships in Orbit', 'viewShipsBody');
+
+function viewShipsBody()
+{
+	global $eol, $mysqli;
+	$userid = $_SESSION['userid'];
+	$userid = $_GET['planetid'];
+
+	$query = $mysqli->prepare('SELECT count,shipname FROM fleets LEFT JOIN fleetships USING (fleetid) LEFT JOIN shipdesigns USING (designid) WHERE userID = ? AND planetid = ? AND orderid = 0');
+	if (!$query)
+	{
+		echo 'error: ', $mysqli->error, $eol;
+		exit;
+	}
+	$query->bind_param('ii', $userid, $planetid);
+
+	$result = $query->execute();
+	if (!$result)
+	{
+		echo 'error: ', $query->error, $eol;
+		exit;
+	}
+
+	$query->bind_result($count,$shipname);
+
+	echo '<table>', $eol;
+	echo '<tr><th>Design Name</th><th>Count</th></tr>', $eol;
+
+	if($query->fetch())
+	{
+		do
+		{
+			echo '<tr>';
+			echo "<td>$shipname</td>";
+			echo "<td>$count</td>";
+			echo '</tr>', $eol;
+		} while ($query->fetch());
+	}
+	else
+	{
+		echo '<tr><td colspan="2">None!</td></tr>';
+	}
+	echo '</table>', $eol;
+	echo '<a href="buiild_ships.php">Build</a> more ships.', $eol;
+}
+?>
