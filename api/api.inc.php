f1b2d4287af02c1dfa48d0f9e0a7d84072c92ccd
markbeazley <mark@markbeazley.co.uk>
2009-04-08 11:23:09 +0000

    Right order of calls the $mysqli functions helps

--- api/api.inc.php
+++ api/api.inc.php
@@ -13,10 +13,6 @@ function getColonies($apikey){
 		exit;
 	}
 	$query->bind_param('s', $apikey);
-	$query->bind_result($userid);
-	$query->fetch();
-	$query->close();
-
 	$result = $query->execute();
 	if (!$result)
 	{
@@ -25,6 +21,8 @@ function getColonies($apikey){
 	}
 
 	$query->bind_result($userid);
+	$query->fetch();
+	$query->close();
 	
 	$query = $mysqli->prepare('SELECT colonies.planetid,systemid,systems.x,systems.y,planets.orbit,planets.type,colonies.metal,colonies.maxmetal,colonies.metalproduction,colonies.deuterium,colonies.maxdeuterium,colonies.deuteriumproduction,colonies.energy,colonies.maxenergy,colonies.energyproduction FROM colonies LEFT JOIN planets USING (planetid) LEFT JOIN systems USING (systemid) WHERE colonies.userID = ? ORDER BY colonylevel DESC;');
 	if (!$query)
