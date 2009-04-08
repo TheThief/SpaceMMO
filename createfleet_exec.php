555e40ef15f508f6e24db2aeca911bfa752a48f0
TheThief <thief@dynamicarcade.co.uk>
2009-04-08 21:59:40 +0000

    new not needed for arrays

--- createfleet_exec.php
+++ createfleet_exec.php
@@ -54,7 +54,7 @@ function fleetOrderBody()
 	$query->execute();
 	$query->bind_result($designid,$count,$size,$engines,$fuel);
 
-	$planetships = new array();
+	$planetships = array();
 	while ($query->fetch())
 	{
 		$planetships[$designid] = $count;
