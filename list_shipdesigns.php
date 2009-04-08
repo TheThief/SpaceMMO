d7ce93f99dbdb37fb0601bba81f36c0af4dff7bb
TheThief <thief@dynamicarcade.co.uk>
2009-04-08 22:53:58 +0000

    new range formula

--- list_shipdesigns.php
+++ list_shipdesigns.php
@@ -41,7 +41,7 @@ function designListBody()
 			echo "<td>$size</td>";
 			echo "<td>$engines/$fuel/$weapons/$shields/$cargo</td>";
 			echo '<td>', number_format(($engines*4)/$size *6,2), ' PC/h</td>';
-			echo '<td>', number_format(($fuel*6)/$engines,2), ' PC</td>';
+			echo '<td>', number_format(($fuel*(($engines*4)/$size *6))/$engines,2), ' PC</td>';
 			echo '<td>', number_format($weapons), '</td>';
 			echo '<td>', number_format(($shields*10)+$size), ' HP</td>';
 			echo '<td>', number_format($cargo*10), ' m3</td>';
