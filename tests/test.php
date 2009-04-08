b23c8b0e89208eba8ad871b33d212b89f1b09558
markbeazley <mark@markbeazley.co.uk>
2009-04-08 15:04:45 +0000

    Testing vars

--- tests/test.php
+++ tests/test.php
@@ -3,7 +3,7 @@ include "../includes/start.inc.php";
 //prodDropdown(0.5,1,1);
 //prodDropdown(1,1,1);
 //echo formatSeconds("h:i:n",0);
-
+echo $_SERVER["PHP_SELF"];
 foreach (range(0.0, 1.0, 0.1) as $number) {
     echo $number . "\n";
 }
