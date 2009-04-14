e7649056e5ebde7851ef20b6d0298a02ee49e029
TheThief <thief@dynamicarcade.co.uk>
2009-04-14 22:29:43 +0000

    tesing...

--- tests/testpost.php
+++ tests/testpost.php
@@ -1,6 +1,11 @@
 <?php
 include_once "../includes/start.inc.php";
 
+?><html>
+<head>
+</head>
+<body>
+<?
 foreach($_POST['test'] as $key => $value)
 {
 	echo $key,' = ',$value, ' (',mb_strlen($value),' c, ',strlen($value),' b)','<br>', $eol;
@@ -14,10 +19,12 @@ unset($_POST['test']['a'],$_POST['test']['b'],$_POST['test']['c'],$_POST['test']
 echo count($_POST['test']);
 
 ?>
-<form action="testpost.php" method="post" accept-charset="utf-8">
+<form action="testpost.php" method="post" accept-charset="UTF-8">
 <input name="test[a]" type="text"><br>
 <input name="test[b]" type="text"><br>
 <input name="test[c]" type="text"><br>
 <input name="test[d]" type="text"><br>
 <input type="submit">
 </form>
+</body>
+</html>
