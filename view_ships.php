0d6427675430918b97a02f44c54ddd880d4e8000
TheThief <thief@dynamicarcade.co.uk>
2009-04-16 16:11:12 +0000

    format numbers to 2dp

--- view_ships.php
+++ view_ships.php
@@ -98,10 +98,10 @@ function viewShipsBody()
 			echo '<form action="fleetorder_exec.php" method="post">', $eol;
 			echo '<input type="hidden" name="fleet" value="',$fleetid,'">', $eol;
 			echo '<h3>',$lookups['order'][1],' ',systemcode($systemid,$orbit),'</h3>', $eol;
-			echo 'Speed: ',$speed,' PC/h (Fuel use: ',$fueluse*6,' D/h)<br>', $eol;
-			echo 'Fuel: ',$fuel,'/ ',$totalfuelbay,' D<br>', $eol;
-			echo 'Max Range: ',$speed * floor($totalfuelbay/$fueluse) * 6,' PC<br>', $eol;
-			echo 'Cargo Capacity: ',$totalcargobay,' m3<br>', $eol;
+			echo 'Speed: ',number_format($speed,2),' PC/h (Fuel use: ',$fueluse*6,' D/h)<br>', $eol;
+			echo 'Fuel: ',$fuel,' / ',$totalfuelbay,' D<br>', $eol;
+			echo 'Max Range: ',number_format($speed * floor($totalfuelbay/$fueluse) * 6, 2),' PC<br>', $eol;
+			echo 'Cargo Capacity: ',$totalcargo,' m3<br>', $eol;
 			echo '<ul>', $eol;
 
 			$queryships->execute();
