c603a6c3c7cd9495177997aac5e0e0f436751768
markbeazley <mark@markbeazley.co.uk>
2009-04-29 18:36:24 +0000

    Add planet changer to sidebar

--- includes/template.inc.php
+++ includes/template.inc.php
@@ -81,6 +81,7 @@ function template($title, $bodyfunc, $menufunc=null, $headerfunc=null)
 		echo '<li>Metal: ',prodSummary('summary_metal', $metal, $maxmetal, $metalprod),'</li>', $eol;
 		echo '<li>Deuterium: ',prodSummary('summary_deuterium', $deuterium, $maxdeuterium, $deuteriumprod),'</li>', $eol;
 		echo '<li>Energy: ',prodSummary('summary_energy', $energy, $maxenergy, $energyprod),'</li>', $eol;
+		planetChanger();
 		echo '</div>', $eol;
 	}
 	echo '</div>', $eol;
