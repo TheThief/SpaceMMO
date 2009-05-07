da0337c326dc2cad26b61548fd0f8aa2fa86e1d6
TheThief <thief@dynamicarcade.co.uk>
2009-05-07 21:34:33 +0000

    fix error

--- admin/updateships.php
+++ admin/updateships.php
@@ -1,5 +1,5 @@
 <?include_once 'includes/admin.inc.php';checkIsAdmin();
-include_once '../includes/ships.inc.php';$mysqli->autocommit(false);$updatequery = $mysqli->prepare('UPDATE shipdesigns SET speed=?,fuelcapacity=?,fueluse=?,cargocapacity=?,defense=? WHERE designid=?');$updatequery->bind_param('iidiii', $speed, $fuelcapacity, $fueluse, $cargocapacity, $defense, $designid);
+include_once '../includes/ships.inc.php';$mysqli->autocommit(false);$updatequery = $mysqli->prepare('UPDATE shipdesigns SET speed=?,fuelcapacity=?,fueluse=?,cargocapacity=?,defense=? WHERE designid=?');$updatequery->bind_param('didiii', $speed, $fuelcapacity, $fueluse, $cargocapacity, $defense, $designid);
 $query = $mysqli->prepare('SELECT designid,size,engines,fuel,cargo,weapons,shields FROM shipdesigns LEFT JOIN shiphulls USING (hullid)');$query->bind_result($designid, $size, $engines, $fuel, $cargo, $weapons, $shields);$query->execute();$query->store_result();while ($query->fetch()){	$speed = speed($size, $engines);	$fuelcapacity = fuelCapacity($fuel);	$fueluse = fuelUse($size, $engines)/SMALLTICKS_PH;	$cargocapacity = cargoCapacity($cargo);	$defense = defense($size, $shields);	$updatequery->execute();}
 
 $mysqli->commit();
