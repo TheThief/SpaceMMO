e7871fb018e475f8a50b25650bf9a9ff18fa8743
TheThief <thief@dynamicarcade.co.uk>
2009-05-07 20:38:31 +0000

    changing fuel and fueluse in a fleet to be decimals with 2 digits to the right of the point, and add a fueluse column to ship designs in preparation for the "engine efficiency by size" change

--- admin/createfleetstable.php
+++ admin/createfleetstable.php
@@ -9,7 +9,7 @@ header('Content-type: text/plain');
 $mysqli = new mysqli($db_server,$db_admin_user,$db_admin_pass,$db_db);
 
 //$result = $mysqli->query('DROP TABLE fleets');
-$result = $mysqli->query('CREATE TABLE fleets(fleetid INT NOT NULL AUTO_INCREMENT, userID INT NOT NULL, planetID INT NOT NULL, orderID INT NOT NULL DEFAULT 0, orderplanetID INT NOT NULL, orderticks INT NOT NULL DEFAULT 0, totalorderticks INT NOT NULL DEFAULT 0, breturnorder BOOL NOT NULL DEFAULT FALSE, speed FLOAT NOT NULL DEFAULT 0, totalcargo INT NOT NULL DEFAULT 0, metal INT NOT NULL DEFAULT 0, deuterium INT NOT NULL DEFAULT 0, totalfuelbay INT NOT NULL DEFAULT 0, fuel INT NOT NULL DEFAULT 0, fueluse INT NOT NULL DEFAULT 0, PRIMARY KEY (fleetID)) ENGINE=INNODB');
+$result = $mysqli->query('CREATE TABLE fleets(fleetid INT NOT NULL AUTO_INCREMENT, userID INT NOT NULL, planetID INT NOT NULL, orderID INT NOT NULL DEFAULT 0, orderplanetID INT NOT NULL, orderticks INT NOT NULL DEFAULT 0, totalorderticks INT NOT NULL DEFAULT 0, breturnorder BOOL NOT NULL DEFAULT FALSE, speed FLOAT NOT NULL DEFAULT 0, totalcargo INT NOT NULL DEFAULT 0, metal INT NOT NULL DEFAULT 0, deuterium INT NOT NULL DEFAULT 0, totalfuelbay INT NOT NULL DEFAULT 0, fuel DECIMAL(11,2) NOT NULL DEFAULT 0, fueluse DECIMAL(8,2) NOT NULL DEFAULT 0, PRIMARY KEY (fleetID)) ENGINE=INNODB');
 if ($result)
 {
 	echo 'table \'fleets\' created successfully', $eol;
