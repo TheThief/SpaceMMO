2eac8525ab7c2f881c3ee8c60a35e30b21c6e406
TheThief <thief@dynamicarcade.co.uk>
2009-05-12 21:40:32 +0000

    *TICKS_PH on colony list

--- list_colonies.php
+++ list_colonies.php
@@ -12,20 +12,8 @@ function colonyListBody()
 	$userid = $_SESSION['userid'];
 
 	$query = $mysqli->prepare('SELECT colonies.planetid,systemid,systems.x,systems.y,planets.orbit,planets.type,colonies.metal,colonies.maxmetal,colonies.metalproduction,colonies.deuterium,colonies.maxdeuterium,colonies.deuteriumproduction,colonies.energy,colonies.maxenergy,colonies.energyproduction FROM colonies LEFT JOIN planets USING (planetid) LEFT JOIN systems USING (systemid) WHERE colonies.userID = ? ORDER BY colonylevel DESC;');
-	if (!$query)
-	{
-		echo 'error: ', $mysqli->error, $eol;
-		exit;
-	}
 	$query->bind_param('i', $userid);
-
-	$result = $query->execute();
-	if (!$result)
-	{
-		echo 'error: ', $query->error, $eol;
-		exit;
-	}
-
+	$query->execute();
 	$query->bind_result($planetid,$systemid,$systemx,$systemy,$orbit,$planettype,$metal,$maxmetal,$metalprod,$deuterium,$maxdeuterium,$deuteriumprod,$energy,$maxenergy,$energyprod);
 
 	echo '<table>', $eol;
@@ -36,9 +24,9 @@ function colonyListBody()
 		echo '<tr>';
 		echo '<td><a href="view_planets.php?system=',$systemid,'">',systemcode($systemid, $orbit),'</a></td>';
 		echo '<td><a href="view_planet.php?planet=',$planetid,'"><img src="images/planet',$planettype,'.png" style="width:1em;height:1em;">',$lookups["planetType"][$planettype],'</a></td>';
-		echo "<td>$metal/$maxmetal (".getSigned($metalprod).")</td>";
-		echo "<td>$deuterium/$maxdeuterium (".getSigned($deuteriumprod).")</td>";
-		echo "<td>$energy/$maxenergy (".getSigned($energyprod).")</td>";
+		echo "<td>$metal/$maxmetal (".getSigned($metalprod*TICKS_PH).")</td>";
+		echo "<td>$deuterium/$maxdeuterium (".getSigned($deuteriumprod*TICKS_PH).")</td>";
+		echo "<td>$energy/$maxenergy (".getSigned($energyprod*TICKS_PH).")</td>";
 		if($_SESSION['colony'] != $planetid){
 			echo '<td><a href="change_colony.php?planet=', $planetid, '">Change to</a></td>';
 		}else{
