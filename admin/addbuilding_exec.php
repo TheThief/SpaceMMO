e6ae53f2bbd4068a36b75094dc27684cd2f2828d
markbeazley <mark@markbeazley.co.uk>
2009-05-13 16:58:27 +0000

    Add support for bignorecolonylevel to admin pages

--- admin/addbuilding_exec.php
+++ admin/addbuilding_exec.php
@@ -5,14 +5,14 @@ checkIsAdmin();
 $eol = "\n";
 header('Content-type: text/plain');
 
-$query = $mysqli->prepare('INSERT INTO buildings (buildingname,mincolonylevel,maxbuildinglevel,buildingdescription,metalcostbase,metalcostlinear,metalcostmultiplier,consumestype,consumesbase,consumeslinear,consumesmultiplier,effecttype,effectbase,effectlinear,effectmultiplier,multiplybyplanet) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)');
+$query = $mysqli->prepare('INSERT INTO buildings (buildingname,mincolonylevel,maxbuildinglevel,buildingdescription,metalcostbase,metalcostlinear,metalcostmultiplier,consumestype,consumesbase,consumeslinear,consumesmultiplier,effecttype,effectbase,effectlinear,effectmultiplier,multiplybyplanet,bignorecolonylevel) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)');
 if (!$query)
 {
 	echo 'error: ', $mysqli->error, $eol;
 	exit;
 }
 
-$query->bind_param('siisiddiiddiiddi', $name, $mincolonylevel, $maxlevel, $description, $costbase, $costlinear, $costmult, $consumestype, $consumesbase, $consumeslinear, $consumesmult, $effecttype, $effectbase, $effectlinear, $effectmult, $multiplybyplanet);
+$query->bind_param('siisiddiiddiiddi', $name, $mincolonylevel, $maxlevel, $description, $costbase, $costlinear, $costmult, $consumestype, $consumesbase, $consumeslinear, $consumesmult, $effecttype, $effectbase, $effectlinear, $effectmult, $multiplybyplanet,$bignorecolonylevel);
 $name = $_POST['name'];
 $mincolonylevel = $_POST['mincolonylevel'];
 $maxlevel = $_POST['maxlevel'];
@@ -29,7 +29,7 @@ $effectbase = $_POST['effectbase'];
 $effectlinear = $_POST['effectlinear'];
 $effectmult = $_POST['effectmult'];
 $multiplybyplanet = (isset($_POST['multiplybyplanet']))?$_POST['multiplybyplanet']:0;
-
+$bignorecolonylevel = (isset($_POST['bignorecolonylevel']))?$_POST['bignorecolonylevel']:0;
 
 $result = $query->execute();
 if (!$result)
