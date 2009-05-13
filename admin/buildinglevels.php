e79cfbb5697d5f5104604f4415fdf625a843022d
markbeazley <mark@markbeazley.co.uk>
2009-05-13 22:17:56 +0000

    Add running cost

--- admin/buildinglevels.php
+++ admin/buildinglevels.php
@@ -4,7 +4,7 @@ include_once '../includes/template.inc.php';template('Building Levels', 'buildi
 	if (!$result)	{		echo 'error: ', $query->error, $eol;		exit;	}
 
 	$query->close();	echo '<h3>', $buildingname, '</h3>', $eol;
-	echo '<table>', $eol;	echo '<tr><th>Level</th><th>Cost</th>', $eol;
+	echo '<table>', $eol;	echo '<tr><th>Level</th><th>Cost</th><th>Cumlative</th>', $eol;
 	if ($consumestype)
 	{
 		echo '<th>', $lookups["resourceType"][$consumestype], ' Use</th>';
@@ -15,14 +15,14 @@ include_once '../includes/template.inc.php';template('Building Levels', 'buildi
 	}
 	echo '</tr>', $eol;	$query = $mysqli->prepare('SELECT building_cost(buildingid,?) AS cost, building_consumes(buildingid,?) AS consumes, building_effect(buildingid,?) AS effect FROM buildings WHERE buildingid = ?');	if (!$query)	{		echo 'error in prepare: ', $mysqli->error, $eol;		exit;	}	$query->bind_param('iiii', $level, $level, $level, $buildingid);
 	$query->bind_result($cost,$consumes,$effect);
-
+	$tcost = 0;
 	for($level=1;$level<=$maxlevel;$level++)
 	{
 		$result = $query->execute();
-		if (!$result)		{			echo 'error in execute: ', $query->error, $eol;			exit;		}		$result = $query->fetch();		if (!$result)		{			echo 'error in fetch: ', $query->error, $eol;			exit;		}
+		if (!$result)		{			echo 'error in execute: ', $query->error, $eol;			exit;		}		$result = $query->fetch();		if (!$result)		{			echo 'error in fetch: ', $query->error, $eol;			exit;		}		$tcost += $cost;
 		echo '<tr>';
 		echo '<td style="text-align: right">', $level, '</td>';
-		echo '<td style="text-align: right">', number_format($cost), '</td>';
+		echo '<td style="text-align: right">', number_format($cost), '</td>';		echo '<td style="text-align: right">', number_format($tcost), '</td>';
 		if ($consumestype)
 		{
 			echo '<td style="text-align: right">', number_format($consumes), '</td>';
