a1b027fb37bf0a41960bc2d8824aff5114d2e07b
markbeazley <mark@markbeazley.co.uk>
2009-05-17 18:44:20 +0000

    GD is missing

--- admin/generate_universe_plot.php
+++ admin/generate_universe_plot.php
@@ -1,7 +1,7 @@
 <?
 //include 'includes/admin.inc.php';
 $scale = 1;
-//header("Content-type: image/png");
+header("Content-type: image/png");
 $img = imagecreatetruecolor(100*$scale,100*$scale);
 $syscolour = imagecolorallocate($img,255,255,255);
 $imgmask = imagecreatefrompng('mask.png');
@@ -118,6 +118,6 @@ foreach($systems as $sys){
 imagesetpixel($img,($sys->x*$scale)+50,($sys->y*$scale)+50,$syscolour);
 }
 
-//imagepng($img);
-//imagedestroy($img);
+imagepng($img);
+imagedestroy($img);
 ?>
