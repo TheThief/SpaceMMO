b787c4dbd23a232d4195619cc4500ca3274fb05e
TheThief <thief@dynamicarcade.co.uk>
2012-03-16 12:37:27 +0000

    trying more

--- default.js
+++ default.js
@@ -1,13 +1,12 @@
 $(document).ready(function(){
     $(".navbutton").click(function(){
         event.preventDefault();
-        var url = $(this).attr("href");
-        if(url.indexOf("?")<0) url = url + "?";
-        url = url+"&api=json";
-        $.getJSON(url,function(data){
+        var hrefurl = $(this).attr("href");
+        var jsonurl = hrefurl + (hrefurl.indexOf("?")<0:"?":"&") + "api=json";
+        $.getJSON(jsonurl,function(data){
             if(data.status=="ok"){
 		if(window.history.replaceState){
-                    window.history.replaceState(null,null,$(this).attr("href"));
+                    window.history.replaceState(null,null,hrefurl);
                 }
 
                 $(".navbutton.up").attr("href",data.navbuttons.up);
