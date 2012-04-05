f675afaf12f0d56a6e768233bc2b0f10ce36fe3c
TheThief <thief@dynamicarcade.co.uk>
2012-04-05 13:28:34 +0000

    Another fix

--- view_systems.php
+++ view_systems.php
@@ -1,176 +1,177 @@
 <?
 include_once 'includes/start.inc.php';
 $isAPICall = false;
-if(isset($_GET["api"]) && $_GET["api"]=="json") $isAPICall = true;
+if(isset($_GET['api']) && $_GET['api']=='json') $isAPICall = true;
 $loggedIn = checkLoggedIn(!$isAPICall);
 
 include_once 'includes/template.inc.php';
 if(!$isAPICall) template('View Galaxy', 'viewSystemsBody');
 if($isAPICall){
-    $results=array();
-    if($loggedIn){
-        $userid = $_SESSION['userid'];
-        $x = $_GET['x'];
-        $y = $_GET['y'];
-        $syscode = $_GET['syscode'];
+	$results=array();
+	if($loggedIn){
+		$userid = $_SESSION['userid'];
+		$x = isset($_GET['x']) ? $_GET['x'] : '';
+		$y = isset($_GET['y']) ? $_GET['y'] : '';
 
-        $viewsize = 30; // in em
-        $minstarsize = 1.2;// in em
-        $maxstarsize = 2;  // in em
-        $gridsize = 2;     // in em
-        $scroll = 3;       // in grid squares
+		$viewsize = 30; // in em
+		$minstarsize = 1.2;// in em
+		$maxstarsize = 2;  // in em
+		$gridsize = 2;     // in em
+		$scroll = 3;       // in grid squares
 
-        $zoom = $_GET['zoom']; if (!is_numeric($zoom)) $zoom=1;
-        $zoom = clamp($zoom, 0.25, 2);
-        $gridsize *= $zoom;
-        $viewdistance = floor(($viewsize/$gridsize - 1) / 2); // in grid squares
-        $indent = ($viewsize - ($viewdistance * 2 + 1) * $gridsize) / 2; // in em
-        $minstarsize *= $zoom;
-        $maxstarsize *= $zoom;
-        $scroll = floor($scroll/$zoom);
+		$zoom = $_GET['zoom']; if (!is_numeric($zoom)) $zoom=1;
+		$zoom = clamp($zoom, 0.25, 2);
+		$gridsize *= $zoom;
+		$viewdistance = floor(($viewsize/$gridsize - 1) / 2); // in grid squares
+		$indent = ($viewsize - ($viewdistance * 2 + 1) * $gridsize) / 2; // in em
+		$minstarsize *= $zoom;
+		$maxstarsize *= $zoom;
+		$scroll = floor($scroll/$zoom);
 
-        if (!is_numeric($x) || !is_numeric($y))
-        {
-            $systemid = $_GET['system'];
-            if(!is_numeric($systemid) && (strlen($syscode)==3 || strlen($syscode)==4)){
-                $systemid = systemid($syscode);
-            }
+		if (!is_numeric($x) || !is_numeric($y))
+		{
+			$systemid = isset($_GET['system']) ? $_GET['system'] : '';
+			$syscode = isset($_GET['syscode']) ? $_GET['syscode'] : '';
+			if (!is_numeric($systemid) && (strlen($syscode) == 3 || strlen($syscode) == 4))
+			{
+				$systemid = systemid($syscode);
+			}
 
-            if (is_numeric($systemid))
-            {
-                $query = $mysqli->prepare('SELECT x,y FROM systems WHERE systemid=?');
-                $query->bind_param('i', $systemid);
-                $query->execute();
-                $query->bind_result($x, $y);
-                $result = $query->fetch();
-                if (!$result)
-                {
-                    $results["status"] = "error";
-                    $results["error"] = "Invalid System ID";
-                    $results["errorCode"] = "INVALIDSYSTEM";
-                    echo json_encode($results);
-                    exit;
-                }
-                $query->close();
-            }
-            else
-            {
-                $query = $mysqli->prepare('SELECT x,y FROM colonies LEFT JOIN planets USING(planetid) LEFT JOIN systems USING(systemid) WHERE userid=? ORDER BY colonylevel DESC LIMIT 1');
-                $query->bind_param('i', $userid);
-                $query->execute();
-                $query->bind_result($x, $y);
-                $result = $query->fetch();
-                if (!$result)
-                {
-                    $results["status"] = "error";
-                    $results["error"] = "You have no colonies";
-                    $results["errorCode"] = "NOCOLONIES";
-                    echo json_encode($results);
-                    exit;
-                }
+			if (is_numeric($systemid))
+			{
+				$query = $mysqli->prepare('SELECT x,y FROM systems WHERE systemid=?');
+				$query->bind_param('i', $systemid);
+				$query->execute();
+				$query->bind_result($x, $y);
+				$result = $query->fetch();
+				if (!$result)
+				{
+					$results['status'] = 'error';
+					$results['error'] = 'Invalid System ID';
+					$results['errorCode'] = 'INVALIDSYSTEM';
+					echo json_encode($results);
+					exit;
+				}
+				$query->close();
+			}
+			else
+			{
+				$query = $mysqli->prepare('SELECT x,y FROM colonies LEFT JOIN planets USING(planetid) LEFT JOIN systems USING(systemid) WHERE userid=? ORDER BY colonylevel DESC LIMIT 1');
+				$query->bind_param('i', $userid);
+				$query->execute();
+				$query->bind_result($x, $y);
+				$result = $query->fetch();
+				if (!$result)
+				{
+					$results['status'] = 'error';
+					$results['error'] = 'You have no colonies';
+					$results['errorCode'] = 'NOCOLONIES';
+					echo json_encode($results);
+					exit;
+				}
 
-                $query->close();
-            }
-        }
+				$query->close();
+			}
+		}
 
-        $x = clamp($x, -UNI_CENTRE_X, UNI_CENTRE_X);
-        $y = clamp($y, -UNI_CENTRE_Y, UNI_CENTRE_Y);
+		$x = clamp($x, -UNI_CENTRE_X, UNI_CENTRE_X);
+		$y = clamp($y, -UNI_CENTRE_Y, UNI_CENTRE_Y);
 
-        $colonyid = $_SESSION['colony'];
-        $query = $mysqli->prepare('SELECT systemid,x,y FROM planets LEFT JOIN systems USING(systemid) WHERE planetid=?');
-        $query->bind_param('i', $colonyid);
-        $query->execute();
-        $query->bind_result($colonysystemid, $colonyx, $colonyy);
-        $result = $query->fetch();
-        if (!$result)
-        {
-            $results["status"] = "error";
-            $results["error"] = "Invalid Colony";
-            $results["errorCode"] = "INVALIDCOLONY";
-            echo json_encode($results);
-            exit;
-        }
-        $query->close();
+		$colonyid = $_SESSION['colony'];
+		$query = $mysqli->prepare('SELECT systemid,x,y FROM planets LEFT JOIN systems USING(systemid) WHERE planetid=?');
+		$query->bind_param('i', $colonyid);
+		$query->execute();
+		$query->bind_result($colonysystemid, $colonyx, $colonyy);
+		$result = $query->fetch();
+		if (!$result)
+		{
+			$results['status'] = 'error';
+			$results['error'] = 'Invalid Colony';
+			$results['errorCode'] = 'INVALIDCOLONY';
+			echo json_encode($results);
+			exit;
+		}
+		$query->close();
 
-        $stmt = $mysqli->prepare('SELECT systemid,x,y,COUNT(user_colonies.planetid),COUNT(other_colonies.planetid) FROM systems LEFT JOIN planets USING (systemid)
-        LEFT JOIN (SELECT planetid FROM colonies WHERE userid=?) user_colonies USING (planetid)
-        LEFT JOIN (SELECT planetid FROM colonies WHERE userid!=?) other_colonies USING (planetid)
-        WHERE x>=? AND x<=? AND y>=? AND y<=? GROUP BY systemid ORDER BY NULL');
-        $xmin = $x-$viewdistance;
-        $xmax = $x+$viewdistance;
-        $ymin = $y-$viewdistance;
-        $ymax = $y+$viewdistance;
-        $stmt->bind_param('iiiiii',$userid,$userid,$xmin,$xmax,$ymin,$ymax);
-        $stmt->execute();
-        $stmt->bind_result($systemid,$sysX,$sysY,$colonies,$othercolonies);
+		$stmt = $mysqli->prepare('SELECT systemid,x,y,COUNT(user_colonies.planetid),COUNT(other_colonies.planetid) FROM systems LEFT JOIN planets USING (systemid)
+		LEFT JOIN (SELECT planetid FROM colonies WHERE userid=?) user_colonies USING (planetid)
+		LEFT JOIN (SELECT planetid FROM colonies WHERE userid!=?) other_colonies USING (planetid)
+		WHERE x>=? AND x<=? AND y>=? AND y<=? GROUP BY systemid ORDER BY NULL');
+		$xmin = $x-$viewdistance;
+		$xmax = $x+$viewdistance;
+		$ymin = $y-$viewdistance;
+		$ymax = $y+$viewdistance;
+		$stmt->bind_param('iiiiii',$userid,$userid,$xmin,$xmax,$ymin,$ymax);
+		$stmt->execute();
+		$stmt->bind_result($systemid,$sysX,$sysY,$colonies,$othercolonies);
 
-        $results["status"] = "ok";
-        $results["navbuttons"]["up"]='view_systems.php?x=' . $x . '&y=' .  ($y-$scroll) . '&zoom='. $zoom;
-        $results["navbuttons"]["down"]='view_systems.php?x=' . $x . '&y=' .  ($y+$scroll) . '&zoom='. $zoom;
-        $results["navbuttons"]["left"]='view_systems.php?x=' . ($x-$scroll) . '&y=' .  $y . '&zoom='. $zoom;
-        $results["navbuttons"]["right"]='view_systems.php?x=' . ($x+$scroll) . '&y=' .  $y . '&zoom='. $zoom;
-        $results["navbuttons"]["zoomin"]='view_systems.php?x=' . $x . '&y=' .  $y . '&zoom='. ($zoom*2);
-        $results["navbuttons"]["zoomout"]='view_systems.php?x=' . $x . '&y=' .  $y . '&zoom='. ($zoom/2);
-        $results["systems"] = array();
-        while ($stmt->fetch())
-        {
-            $image = 'images/star'.($systemid%4 +1).'.png';
-            $image2 = null;
-            $syscode = systemcode($systemid);
-            $systeminfo = 'Not colonised'. $eol;
-            if ($systemid == $colonysystemid)
-            {
-                if ($othercolonies)
-                {
-                    $image2 ='images/star-oc+cc.png';
-                    $systeminfo = 'Current System (Contested).<br/>Your colonies: '.$colonies.'</br/>Other colonies: '.$othercolonies . $eol;
-                }
-                else
-                {
-                    $image2 = 'images/star-c.png';
-                    $systeminfo = 'Current system.<br/>Colonised planets: '.$colonies . $eol;
-                }
-            }
-            else if ($colonies && $othercolonies)
-            {
-                $image2 ='images/star-oc+c.png';
-                $systeminfo = 'Contested System.<br/>Your colonies: '.$colonies.'<br/>Other colonies: '.$othercolonies . $eol;
-            }
-            else if ($colonies)
-            {
-                $image2 = 'images/star-c.png';
-                $systeminfo = 'Your system.<br/>Colonised planets: '.$colonies . $eol;
-            }
-            else if ($othercolonies)
-            {
-                $image2 = 'images/star-oc.png';
-                $systeminfo = 'Enemy system.<br/>Colonised planets: '.$othercolonies . $eol;
-            }
+		$results['status'] = 'ok';
+		$results['navbuttons']['up']      ='view_systems.php?x=' . $x . '&y=' .  ($y-$scroll) . '&zoom='. $zoom;
+		$results['navbuttons']['down']    ='view_systems.php?x=' . $x . '&y=' .  ($y+$scroll) . '&zoom='. $zoom;
+		$results['navbuttons']['left']    ='view_systems.php?x=' . ($x-$scroll) . '&y=' .  $y . '&zoom='. $zoom;
+		$results['navbuttons']['right']   ='view_systems.php?x=' . ($x+$scroll) . '&y=' .  $y . '&zoom='. $zoom;
+		$results['navbuttons']['zoomin']  ='view_systems.php?x=' . $x . '&y=' .  $y . '&zoom='. ($zoom*2);
+		$results['navbuttons']['zoomout'] ='view_systems.php?x=' . $x . '&y=' .  $y . '&zoom='. ($zoom/2);
+		$results['systems'] = array();
+		while ($stmt->fetch())
+		{
+			$image = 'images/star'.($systemid%4 +1).'.png';
+			$image2 = null;
+			$syscode = systemcode($systemid);
+			$systeminfo = 'Not colonised'. $eol;
+			if ($systemid == $colonysystemid)
+			{
+				if ($othercolonies)
+				{
+					$image2 ='images/star-oc+cc.png';
+					$systeminfo = 'Current System (Contested).<br/>Your colonies: '.$colonies.'</br/>Other colonies: '.$othercolonies . $eol;
+				}
+				else
+				{
+					$image2 = 'images/star-c.png';
+					$systeminfo = 'Current system.<br/>Colonised planets: '.$colonies . $eol;
+				}
+			}
+			else if ($colonies && $othercolonies)
+			{
+				$image2 ='images/star-oc+c.png';
+				$systeminfo = 'Contested System.<br/>Your colonies: '.$colonies.'<br/>Other colonies: '.$othercolonies . $eol;
+			}
+			else if ($colonies)
+			{
+				$image2 = 'images/star-c.png';
+				$systeminfo = 'Your system.<br/>Colonised planets: '.$colonies . $eol;
+			}
+			else if ($othercolonies)
+			{
+				$image2 = 'images/star-oc.png';
+				$systeminfo = 'Enemy system.<br/>Colonised planets: '.$othercolonies . $eol;
+			}
 
-            if ($systemid != $colonysystemid)
-            {
-                $systeminfo .= '<br/>Distance: '.number_format(distance($sysX-$colonyx, $sysY-$colonyy), 2).'PC';
-            }
+			if ($systemid != $colonysystemid)
+			{
+				$systeminfo .= '<br/>Distance: '.number_format(distance($sysX-$colonyx, $sysY-$colonyy), 2).'PC';
+			}
 
-            $starsize = (floor($systemid/4)%4)/4 * ($maxstarsize-$minstarsize) + $minstarsize;
-            $temp = Array();
-            $temp["container"] = array("height"=>$gridsize,"width"=>$gridsize,"x"=>($sysX-$xmin)*$gridsize + $indent,"y"=>($sysY-$ymin)*$gridsize + $indent);
-            $temp["link"] = "view_planets.php?system=" . $systemid;
-            $temp["star"] = array("image"=>$image,"width"=>$starsize,"height"=>$starsize,"offsetX"=>0-($starsize/2),"offsetY"=>0-($starsize/2));
-            if ($image2)
-            {
-                $temp["indicator"] = array("image"=>$image2,"width"=>$gridsize,"height"=>$gridsize);
-            }
-            $temp["info"] = array("padding"=>$gridsize+0.3,"syscode"=>$syscode,"info"=>$systeminfo);
-            $results["systems"][] = $temp;
-        }
-        $stmt->close();
-    }else{
-        $results["status"] = "error";
-        $results["error"] = "Not Logged In";
-        $results["errorCode"] = "NOTAUTHED";
-    }
-    echo json_encode($results);
+			$starsize = (floor($systemid/4)%4)/4 * ($maxstarsize-$minstarsize) + $minstarsize;
+			$temp = Array();
+			$temp['container'] = array('height'=>$gridsize,'width'=>$gridsize,'x'=>($sysX-$xmin)*$gridsize + $indent,'y'=>($sysY-$ymin)*$gridsize + $indent);
+			$temp['link'] = 'view_planets.php?system=' . $systemid;
+			$temp['star'] = array('image'=>$image,'width'=>$starsize,'height'=>$starsize,'offsetX'=>0-($starsize/2),'offsetY'=>0-($starsize/2));
+			if ($image2)
+			{
+				$temp['indicator'] = array('image'=>$image2,'width'=>$gridsize,'height'=>$gridsize);
+			}
+			$temp['info'] = array('padding'=>$gridsize+0.3,'syscode'=>$syscode,'info'=>$systeminfo);
+			$results['systems'][] = $temp;
+		}
+		$stmt->close();
+	}else{
+		$results['status'] = 'error';
+		$results['error'] = 'Not Logged In';
+		$results['errorCode'] = 'NOTAUTHED';
+	}
+	echo json_encode($results);
 }
 function viewSystemsBody()
 {
