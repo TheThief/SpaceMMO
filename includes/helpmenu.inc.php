edae2cd71494c831fa4257b8e1f904b44bfee204
TheThief <thief@dynamicarcade.co.uk>
2012-04-13 16:37:24 +0000

    www.dynamicarcade.co.uk/SpaceMMO/ -> spacemmo.dynamicarcade.co.uk
    TODO: Relative paths in a few places that still use absolute paths, i.e. tick.php

--- includes/helpmenu.inc.php
+++ includes/helpmenu.inc.php
@@ -4,7 +4,7 @@ function helpMenu()
 	global $eol;
 	echo '<h3>Help Menu</h3>', $eol;
 	echo '<ul>', $eol;
-	echo '<li><a href="/SpaceMMO/help/getting_started.php">Getting Started</a></li>', $eol;
-	echo '<li><a href="/SpaceMMO/help/colonising.php">Colonising</a></li>', $eol;
+	echo '<li><a href="/help/getting_started.php">Getting Started</a></li>', $eol;
+	echo '<li><a href="/help/colonising.php">Colonising</a></li>', $eol;
 	echo '</ul>', $eol;
 }
